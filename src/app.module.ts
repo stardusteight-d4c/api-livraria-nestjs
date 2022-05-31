5e5fc06b2c343dba6bb90506f4140b18a3cbb3ea
Gabriel <bellsena@outlook.com>
2022-05-31 10:35:52 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Livro } from './livro.model';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.USER_DB,
+      password: process.env.PASSWORD_DB,
+      database: 'livraria',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
+})
+export class AppModule {}
